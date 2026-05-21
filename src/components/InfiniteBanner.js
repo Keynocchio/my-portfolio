fd84ed4ce6cd54c86535c58581a58fc11221a863
Keynocchio <keynocchio@gmail.com>
2026-05-20 22:28:07 -0400

    d

--- src/components/InfiniteBanner.js
+++ src/components/InfiniteBanner.js
@@ -4,18 +4,27 @@ import { useEffect, useState } from "react";
 
 export default function InfiniteBanner() {
   const [artworks, setArtworks] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     fetch("/images/display-artwork/list.json")
       .then((res) => res.json())
-      .then(setArtworks);
+      .then((data) => {
+        setArtworks(data);
+
+        // force mobile layout recalculation
+        requestAnimationFrame(() => {
+          window.dispatchEvent(new Event("resize"));
+          setLoaded(true);
+        });
+      });
   }, []);
 
   const looped = [...artworks, ...artworks];
 
   return (
     <section className="overflow-hidden py-10">
-      <div className="banner-track">
+      <div className={`banner-track ${loaded ? "loaded" : ""}`}>
         {looped.map((file, i) => (
           <img
             key={i}
