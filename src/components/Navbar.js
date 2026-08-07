c0a98292fe8bd47bf8fb60896073b57b53f2ad4b
David Valcarcel <keynocchio@gmail.com>
2026-08-06 22:15:17 -0400

    dd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 const navItems = [
-  { href: "/", label: "Home" },
+  { href: "https://keynocchio.com/", label: "Home" },
   { href: "/portfolio-and-artworks", label: "Portfolio & Artworks" },
   { href: "/comics", label: "Comics" },
   { href: "https://keynocchio.itch.io/", label: "Games", external: true },
